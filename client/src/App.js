7d5b9aafd87485fe407f1c25e9098f07a967fb43
Flavian Mohanraj <37390414+flave19@users.noreply.github.com>
2020-03-13 17:23:31 -0400

    header bar added (#2)
    Co-authored-by: Flavian <flavian.mohanraj@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CalendarSelector from '../src/components/Calendar';
+import HeaderBar from '../src/components/Header'
 import { Grommet } from "grommet";
 
 function App() {
   return (
+    
     <Grommet>
+       <HeaderBar></HeaderBar>
       <CalendarSelector/>
     </Grommet>
   );
