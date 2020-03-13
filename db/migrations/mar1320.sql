5cf7b5d0f240c285ca0417969096d9bf810ed9f6
cspraggett <cspraggett@gmail.com>
2020-03-13 17:43:57 -0400

    finish initial migration

--- db/migrations/mar1320.sql
+++ db/migrations/mar1320.sql
@@ -1,17 +1,21 @@
 DROP TABLE IF EXISTS admins CASCADE;
 DROP TABLE IF EXISTS employees CASCADE;
 DROP TABLE IF EXISTS availability CASCADE;
+DROP TABLE IF EXISTS schedules CASCADE;
+DROP TABLE IF EXISTS days CASCADE;
+DROP TABLE IF EXISTS shifts CASCADE;
+DROP TABLE IF EXISTS employeeShits CASCADE;
 CREATE TABLE admins (id SERIAL PRIMARY KEY NOT NULL);
 CREATE TABLE employees (
   id SERIAL PRIMARY KEY NOT NULL,
-  admin_id INTEGER REFERENCES admin(id) ON DELETE CASCADE,
+  admin_id INTEGER REFERENCES admins(id) ON DELETE CASCADE,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(250) NOT NULL
 );
 CREATE TABLE availability (
   id SERIAL PRIMARY KEY NOT NULL,
-  employee_id VARCHAR(50) REFERENCES employees(id),
+  employee_id INTEGER REFERENCES employees(id),
   start_time TIME NOT NULL,
   duration INTEGER NOT NULL,
   day_of_week VARCHAR(10)
@@ -26,6 +30,15 @@ CREATE TABLE days (
   day VARCHAR(10),
   schedule_id INTEGER REFERENCES schedules(id)
 );
+CREATE TABLE shifts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  day_id INTEGER REFERENCES days(id),
+  start_time TIME NOT NULL,
+  duration INTEGER,
+  num_of_employess INTEGER
+);
 CREATE TABLE employeeShits (
   employee_id INTEGER REFERENCES employees(id),
-)
+  shift_id INTEGER REFERENCES shifts(id),
+  PRIMARY KEY(employee_id, shift_id)
+);
