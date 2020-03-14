7dd86c29047992edcdd46854d2546a2d25fbd40b
Flavian <flavian.mohanraj@gmail.com>
2020-03-14 13:29:21 -0400

    editbutton added

---
+++ client/src/components/EditButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "grommet";
+import { Edit }from "grommet-icons";
+
+
+export default function EditButton(props) {
+  return (
+    <Button
+  icon={<Edit />}
+  label="Edit"
+  onClick={() => {}}
+    />
+  )
+}
