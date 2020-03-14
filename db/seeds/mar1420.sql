a5a9474fa0ab72183887d93cf916b3a69531c736
cspraggett <cspraggett@gmail.com>
2020-03-14 15:52:27 -0400

    fix seeds

--- db/seeds/mar1420.sql
+++ db/seeds/mar1420.sql
@@ -26,20 +26,20 @@ values
   (2, '09:00', 8, 2),
   (3, '09:00', 8, 2),
   (4, '09:00', 8, 2),
-  (5, '09:00', 8, 3),
+  (5, '09:00', 8, 3);
 INSERT INTO employeeshits (employee_id, shift_id)
 values
   (1, 1),
+  (2, 1),
+  (1, 2),
   (2, 2),
-  (1, 3),
-  (2, 4),
-  (3, 5),
-  (4, 6),
-  (3, 7),
-  (4, 8),
-  (1, 9),
-  (2, 10),
-  (4, 11);
+  (3, 3),
+  (4, 3),
+  (3, 4),
+  (4, 4),
+  (1, 5),
+  (2, 5),
+  (4, 5);
 INSERT INTO availability (employee_id, start_time, duration, day_of_week)
 VALUES
   (1, '09:00', 8, 'm'),
