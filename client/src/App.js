a1cb4307bca6fd0eabab139c20e369af28d4d26a
Henry Boyd <58610551+HenryBoyd4275@users.noreply.github.com>
2020-03-14 13:47:52 -0400

    schedule view and shift component static skeletons added (#6)

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,40 @@ import { Grommet, Box } from "grommet";
 import CalendarSelector from './components/CalendarSelector';
 import EmployeeSearch from './components/EmployeeSearch';
 import EmployeeList from './components/EmployeeList';
-import AddButton from './components/AddButton'
-import HeaderBar from '../src/components/Header'
+import AddButton from './components/AddButton';
+import HeaderBar from '../src/components/Header';
+import ScheduleView from "./components/ScheduleView"
 
 function App() {
   return (
     
     <Grommet>
       <HeaderBar></HeaderBar>
-      <Box 
-        width={"small"}
-        align="center"
-        border={
-                  {
-                  size: "small",
-                  color: "brand",
+      <Box direction="row">
+        <Box 
+          width={"small"}
+          align="center"
+          border={
+                    {
+                    size: "small",
+                    color: "brand",
+                    }
                   }
-                }
-      >
-        <CalendarSelector/>
-        <EmployeeSearch/>
-        <EmployeeList/>
-        <AddButton/>
+        >
+          <CalendarSelector/>
+          <EmployeeSearch/>
+          <EmployeeList/>
+          <AddButton/>
+        </Box>
+        <Box direction="row">
+          <ScheduleView day="Monday"/>
+          <ScheduleView day="Tuesday"/>
+          <ScheduleView day="Wednesday"/>
+          <ScheduleView day="Thursday"/>
+          <ScheduleView day="Friday"/>
+          <ScheduleView day="Saturday"/>
+          <ScheduleView day="Sunday"/>
+        </Box>
       </Box>
     </Grommet>
   );
---
+++ client/src/components/ScheduleView.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { List, Box, Text } from "grommet";
+import AddButton from "./AddButton";
+import Shift from "./Shift";
+
+export default function ScheduleView(props){
+  return(
+    <Box>
+      <Text>{props.day}</Text>
+      <List
+      border={true}
+      primaryKey="object"
+      data={[
+        {object:<Shift start={9} duration={8} capacity={4}/>},
+        {object:"other shift data"},
+        {object:<AddButton/>}
+      ]}
+      />
+    </Box>
+  );
+}
