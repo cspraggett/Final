115c4ad9a0b827f51d7825fbeef0719ae225c82b
cspraggett <55895293+cspraggett@users.noreply.github.com>
2020-03-16 16:40:43 -0400

    Do routes (#10)
    * add .env and connect to database with it
    
    * fetch data from api server
    
    * work on routes
    
    * add axios and .env

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,69 @@
-import React from 'react';
-import { Grommet, Box, Layer } from "grommet";
-import CalendarSelector from './components/CalendarSelector';
-import EmployeeSearch from './components/EmployeeSearch';
-import EmployeeList from './components/EmployeeList';
-import AddButton from './components/AddButton';
-import HeaderBar from './components/Header';
-import ScheduleView from './components/ScheduleView';
-import AddEmployee from './components/AddEmployee';
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Grommet, Box } from "grommet";
+import CalendarSelector from "./components/CalendarSelector";
+import EmployeeSearch from "./components/EmployeeSearch";
+import EmployeeList from "./components/EmployeeList";
+import AddButton from "./components/AddButton";
+import HeaderBar from "../src/components/Header";
+import ScheduleView from "./components/ScheduleView";
 
 function App() {
-  const [show, setShow] = React.useState();
+  const [people, setPeople] = useState(["bob"]);
+  console.log("hello");
+  useEffect(() => {
+    // fetch("http://localhost:5000/employees")
+    //   .then(result => {
+    //     console.log("in fetch", result);
+    //     return result.json();
+    //   })
+    //   .then(data => {
+    //     console.log("data:", data);
+    //     setPeople(data);
+    //   })
+    // .catch(err => console.log("error", err));
+    // const tester = { test: "hello" };
+    // fetch("http://localhost:5000/employees", {
+    //   method: "post",
+    //   headers: new Headers(),
+    //   body: JSON.stringify(tester)
+    // }).then(result => console.log("in then:", result));
+    axios
+      .get("http://localhost:Mondaymployees")
+      .then(response => setPeople(response.data))
+      .catch(error => console.log("in error", error));
+  }, []);
+
 
   return (
-    
     <Grommet>
+      <h1>{people[0].first_name}</h1>
       <HeaderBar></HeaderBar>
       <Box direction="row">
-        <Box 
+        <Box
           width={"small"}
           align="center"
-          border={
-                    {
-                    size: "small",
-                    color: "brand",
-                    }
-                  }
+          border={{
+            size: "small",
+            color: "brand"
+          }}
         >
+
           <CalendarSelector/>
           <EmployeeSearch/>
           <EmployeeList/>
           <AddButton onClick={() => setShow(true)}/>
+
         </Box>
         <Box direction="row">
-          <ScheduleView day="Monday"/>
-          <ScheduleView day="Tuesday"/>
-          <ScheduleView day="Wednesday"/>
-          <ScheduleView day="Thursday"/>
-          <ScheduleView day="Friday"/>
-          <ScheduleView day="Saturday"/>
-          <ScheduleView day="Sunday"/>
+          <ScheduleView day="Monday" />
+          <ScheduleView day="Tuesday" />
+          <ScheduleView day="Wednesday" />
+          <ScheduleView day="Thursday" />
+          <ScheduleView day="Friday" />
+          <ScheduleView day="Saturday" />
+          <ScheduleView day="Sunday" />
         </Box>
       </Box>
       {show && (
--- routes/index.js
+++ routes/index.js
@@ -1,89 +1,105 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../db/index')
+const client = require("../db/index");
 
-pool.connect()
-
-router.get('/employees', (req, res) => {
-  console.log('in get');
-  pool.query(`
+router.get("/employees", (req, res) => {
+  client
+    .query(
+      `
     select * from employees;
-  `)
+  `
+    )
     .then(result => {
-      console.log(result.rows)
-      res.json(result.rows)
+      console.log(result.rows);
+      res.json(result.rows);
     });
-})
+});
 
-router.get('/shifts', (req, res) => {
-  console.log('in get');
-  pool.query(`
+router.get("/shifts", (req, res) => {
+  client
+    .query(
+      `
     select * from shifts;
-  `)
+  `
+    )
     .then(result => {
-      console.log(result.rows)
-      res.json(result.rows)
+      console.log(result.rows);
+      res.json(result.rows);
     });
-})
+});
 
-router.get('/admins', (req, res) => {
-  console.log('in get');
-  pool.query(`
+router.get("/admins", (req, res) => {
+  client
+    .query(
+      `
     select * from admins;
-  `)
+  `
+    )
     .then(result => {
-      console.log(result.rows)
-      res.json(result.rows)
+      console.log(result.rows);
+      res.json(result.rows);
     });
-})
+});
 
-router.get('/availability', (req, res) => {
-  console.log('in get');
-  pool.query(`
+router.get("/availability", (req, res) => {
+  client
+    .query(
+      `
     select * from availability;
-  `)
+  `
+    )
     .then(result => {
-      console.log(result.rows)
-      res.json(result.rows)
+      // console.log(result.rows);
+      res.json(result.rows);
     });
-})
+});
 
-router.get('/days', (req, res) => {
-  console.log('in get');
-  pool.query(`
+router.get("/days", (req, res) => {
+  client
+    .query(
+      `
     select * from days;
-  `)
+  `
+    )
     .then(result => {
-      console.log(result.rows)
-      res.json(result.rows)
+      console.log(result.rows);
+      res.json(result.rows);
     });
-})
+});
 
-router.get('/employeeshifts', (req, res) => {
-  console.log('in get');
-  pool.query(`
+router.get("/employeeshifts", (req, res) => {
+  client
+    .query(
+      `
     select * from days;
-  `)
+  `
+    )
     .then(result => {
-      console.log(result.rows)
-      res.json(result.rows)
+      console.log(result.rows);
+      res.json(result.rows);
     });
-})
+});
 
-router.get('/schedules', (req, res) => {
-  console.log('in get');
-  pool.query(`
+router.get("/schedules", (req, res) => {
+  client
+    .query(
+      `
     select * from schedules;
-  `)
+  `
+    )
     .then(result => {
-      console.log(result.rows)
-      res.json(result.rows)
+      console.log(result.rows);
+      res.json(result.rows);
     });
-})
+});
 
-router.get('/', (req, res) => {
-  console.log('in / ');
-  res.send('Holla');
+router.get("/", (req, res) => {
+  console.log("in / ");
+  res.send("Holla");
+});
+
+router.post("/employees", (req, res) => {
+  console.log(req.body.test);
 });
 
 module.exports = router;
