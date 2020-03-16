4f3eb7a5890ac6ca1667b516cf1ed008cd0ef40f
cspraggett <cspraggett@gmail.com>
2020-03-16 16:29:50 -0400

    add test.js with state data structure

---
+++ test.js
@@ -0,0 +1,18 @@
+//[employees], [weeks], ]
+modal component gets passed through prop 
+display show : true || false toggle
+
+
+const state ={
+  employees:{
+    
+  }
+}modal component gets passed through prop 
+display show : true || false toggle
+
+
+const state ={
+  employees:{
+    
+  }
+}
