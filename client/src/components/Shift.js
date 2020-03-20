177ce36e02313f4a51d089fad92d2f70f8043cab
AmorphousCube <amorphouscube@gmail.com>
2020-03-20 12:57:18 -0400

    dropdown options now fitered based on db info. does not update when you select an option.

--- client/src/components/Shift.js
+++ client/src/components/Shift.js
@@ -5,27 +5,35 @@ import { Box, Text, Select } from "grommet";
 export default function Shift(props){
 
   let employeeNameList = [];
-  
+
   Object.values(props.allEmployees).forEach(element => {
     employeeNameList.push(element.name);
   });
-  console.log(employeeNameList);
 
-  let currentlyShowing = Object.values(props.assignedEmployees);
+  let currentlyShowing = []
+  props.assignedEmployees.forEach((element) => {
+    currentlyShowing.push(element.name);
+  })
+
+  let filteredOptions = employeeNameList.filter(
+    (element) => {
+      return !currentlyShowing.includes(element);
+    }
+  );
 
   function dropDownTable(){
     let result = [];
     let show = "None";
     for(let i = 0; i < props.capacity; i++){
       if(currentlyShowing[i]) {
-        show = currentlyShowing[i].name
+        show = currentlyShowing[i]
       } else{
         show = "None";
       }
       result.push(
         <Select
           value={show}
-          options={employeeNameList}
+          options={filteredOptions}
           onChange={({option}) => currentlyShowing[i]=option}
         />
       )
