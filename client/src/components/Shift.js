d645d23b8bdf1cd3e786baaa2a564eac7da8affa
cspraggett <cspraggett@gmail.com>
2020-03-23 15:14:19 -0400

    correct datatype of employee array

--- client/src/components/Shift.js
+++ client/src/components/Shift.js
@@ -10,7 +10,7 @@ export default function Shift(props) {
       for (let [key, value] of Object.entries(props.allEmployees)) {
         //console.log(`element:${element}, ${key}: ${value}`);
         if (value.name === element) {
-          result.push(key);
+          result.push(parseInt(key));
           console.log(`found ${element} === ${value.name}`);
         }
       }
