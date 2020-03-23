a0c0ed77b8ddb7bb90b423713270c3a9a363bfde
cspraggett <cspraggett@gmail.com>
2020-03-22 20:49:26 -0400

    fix convertshift to supply shift id to state

--- client/src/components/Shift.js
+++ client/src/components/Shift.js
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
-import { Box, Text, Select, Button} from "grommet";
+import { Box, Text, Select, Button } from "grommet";
 import { Save } from "grommet-icons";
 
-export default function Shift(props){
-
+export default function Shift(props) {
   //get back the ID from the strings
-  function idArray(strings){
+  function idArray(strings) {
     let result = [];
     strings.forEach(element => {
       for (let [key, value] of Object.entries(props.allEmployees)) {
         //console.log(`element:${element}, ${key}: ${value}`);
-        if (value.name === element){
+        if (value.name === element) {
           result.push(key);
-          console.log(`found ${element} === ${value.name}`)
+          console.log(`found ${element} === ${value.name}`);
         }
       }
     });
     //console.log("result:", result)
-    return result
-  };
+    return result;
+  }
 
   //creates an array of all employee name strings
   let employeeNameList = [];
@@ -27,59 +26,65 @@ export default function Shift(props){
   });
 
   //creates a array of strings that displays the names already assigned to the shift
-  let currentlyShowing = []
-  props.assignedEmployees.forEach((element) => {
+  let currentlyShowing = [];
+  props.assignedEmployees.forEach(element => {
     currentlyShowing.push(element.name);
-  })
+  });
 
   //filters selectable names by removing ones already in use
-  let filteredOptions = employeeNameList.filter(
-    (element) => {
-      return !currentlyShowing.includes(element);
-    }
-  );
+  let filteredOptions = employeeNameList.filter(element => {
+    return !currentlyShowing.includes(element);
+  });
 
   //the actual dropdown element
-  function dropDownTable(){
+  function dropDownTable() {
     let result = [];
     let show = "None";
     //one dropdown selecter per shift capacity
-    for(let i = 0; i < props.capacity; i++){
-      if(currentlyShowing[i]) {
-        show = currentlyShowing[i]
-      } else{//display "none" if there is no employee assigned to this "slot"
+    for (let i = 0; i < props.capacity; i++) {
+      if (currentlyShowing[i]) {
+        show = currentlyShowing[i];
+      } else {
+        //display "none" if there is no employee assigned to this "slot"
         show = "None";
       }
       result.push(
         <Select
           value={show}
-          options={filteredOptions}//display the filtered names as the options
-          onChange={({option}) => {//callback to run when a dropdown item is selected
-            currentlyShowing[i]=option;
+          options={filteredOptions} //display the filtered names as the options
+          onChange={({ option }) => {
+            //callback to run when a dropdown item is selected
+            currentlyShowing[i] = option;
           }}
         />
-      )
+      );
     }
     return result;
   }
 
-  return(
+  return (
     <Box>
-      {dropDownTable()} 
+      {dropDownTable()}
       <Box direction="row">
-      <Text>{props.start} - {(props.start+props.duration > 12) ? (props.start+props.duration-12): props.start+props.duration}</Text>
-      <Button 
-        onClick={ () => {
-          props.updateShifts({
-            [props.id]: {
-              "employees": idArray(currentlyShowing)
-            }
-          })
-        }} 
-        alignSelf="end" 
-        icon={<Save/>}/>
+        <Text>
+          {props.start} -{" "}
+          {props.start + props.duration > 12
+            ? props.start + props.duration - 12
+            : props.start + props.duration}
+        </Text>
+        <Button
+          onClick={() => {
+            console.log("in updateShifts");
+            props.updateShifts({
+              [props.id]: {
+                employees: idArray(currentlyShowing)
+              }
+            });
+          }}
+          alignSelf="end"
+          icon={<Save />}
+        />
       </Box>
     </Box>
   );
 }
-
