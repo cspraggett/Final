77347bfad6cec475e122da083965cb41a53dd662
AmorphousCube <amorphouscube@gmail.com>
2020-03-30 14:25:24 -0400

    changes

--- client/src/components/Shift.js
+++ client/src/components/Shift.js
@@ -2,54 +2,41 @@ import React, { useState } from "react";
 import { Box, Text, Select, Button } from "grommet";
 import { Save } from "grommet-icons";
 
-//TODO make currently showing state beloning to this component
-
 export default function Shift(props) {
-
-  //creates a array of strings that displays the names already assigned to the shift
-  function setupSelect() {
-    let currentlyShowing = {};
-    props.assignedEmployees.forEach((element, index) => {
-      currentlyShowing = {[index]: element.name};
-    });
-    console.log("setup output:", currentlyShowing);
-    return currentlyShowing;
-  }
-
-  const [selectOptions, setSelectOptions] = useState(setupSelect());
-
-  //creates an array of all employee name strings
-  let employeeNameList = [];
-  function setupNamelist() {
-      Object.values(props.allEmployees).forEach(element => {
-      employeeNameList.push(element.name);
-    });
-  }
-
-  setupNamelist();
+  const [selectOptions, setSelectOptions] = useState({});
 
   //get back the ID from the strings
   function idArray(strings) {
     let result = [];
     strings.forEach(element => {
       for (let [key, value] of Object.entries(props.allEmployees)) {
-        //console.log(`element:${element}, ${key}: ${value}`);
         if (value.name === element) {
           result.push(key);
           //console.log(`found ${element} === ${value.name}`);
         }
       }
     });
-    //console.log("result:", result)
     return result;
   }
 
+  //creates an array of all employee name strings
+  let employeeNameList = [];
+  Object.values(props.allEmployees).forEach(element => {
+    employeeNameList.push(element.name);
+  });
+
+  //creates a array of strings that displays the names already assigned to the shift
+  let currentlyShowing = [];
+  props.assignedEmployees.forEach((element) => {
+    currentlyShowing.push(element.name);
+    //setSelectOptions({...selectOptions, element.name});
+  });
+  console.log("currentlyShowing:", currentlyShowing, "selectOptions:", selectOptions);
+
   //filters selectable names by removing ones already in use
-  function filteredOptions() {
-    return employeeNameList.filter(element => {
-      return !Object.values(selectOptions).includes(element);
-    });
-  } 
+  let filteredOptions = employeeNameList.filter(element => {
+    return !Object.values(selectOptions).includes(element);
+  });
 
   //the actual dropdown element
   function dropDownTable() {
@@ -66,10 +53,9 @@ export default function Shift(props) {
       result.push(
         <Select
           value={show}
-          options={filteredOptions()} //display the filtered names as the options
+          options={filteredOptions} //display the filtered names as the options
           onChange={({ option }) => {
             setSelectOptions({...selectOptions, [i]: option});
-            //console.log(selectOptions);
             //callback to run when a dropdown item is selected
             //currentlyShowing[i] = option;
           }}
@@ -94,7 +80,7 @@ export default function Shift(props) {
             console.log("in updateShifts");
             props.updateShifts({
               [props.id]: {
-                employees: idArray(setupSelect())
+                employees: idArray(currentlyShowing)
               }
             });
           }}
