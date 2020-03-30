28733ba9d7d3087750242e91ddb7a6f2eddaf96f
cspraggett <cspraggett@gmail.com>
2020-03-30 19:37:03 -0400

    stop clicking on employee on the left nav bar causing an error

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { List } from "grommet";
 
-export default function EmployeeList(props){
+export default function EmployeeList(props) {
   return (
-  <List
-  onClickItem={({item}) => {
-    props.onClick(item)
-  }}
-    primaryKey="name"
-    data={props.emp}
-  />
-  )
+    <List
+      // onClickItem={({item}) => {
+      //   props.onClick(item)
+      // }}
+      primaryKey="name"
+      data={props.emp}
+    />
+  );
 }
